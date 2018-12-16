99d888ebd2f48894d31ee04912cfbe29b256b26b
tehgarra <15840963+tehgarra@users.noreply.github.com>
2018-12-16 05:09:15 -0500

    Update UsrFollow.py

--- UsrFollow.py
+++ UsrFollow.py
@@ -7,11 +7,11 @@ client = pytumblr.TumblrRestClient(
   ''
 )
  #Variable Declaration
-count = 0
-osNum = 0
+count = 0 #general counter
+osNum = 0 # offset value
  #Print list of blogs using loop
 while (count <= 19) :
-  print(client.following(offset=osNum)['blogs'][count]['url'])
+  print(client.following(offset=osNum)['blogs'][count]['url']) #note1
   count = count + 1
    if (count > 19) :
     count = 0
@@ -20,3 +20,5 @@ while (count <= 19) :
 # ask for authentication
 # check for total_blogs from usr
 #...
+
+#note1: possible bug, or inential decrease of displayed blogs starting after offset 57
